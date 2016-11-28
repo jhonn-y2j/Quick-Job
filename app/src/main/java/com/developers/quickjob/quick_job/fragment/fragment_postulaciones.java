020aa9508125604ac70f9813d851db929873707a
Jhonn Ayala Chero <y2jz28@gmail.com>
2016-11-28 16:52:52 -0500

    ver postulaciones por parte del postulante hecha

--- app/src/main/java/com/developers/quickjob/quick_job/fragment/fragment_postulaciones.java
+++ app/src/main/java/com/developers/quickjob/quick_job/fragment/fragment_postulaciones.java
@@ -5,15 +5,29 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.developers.quickjob.quick_job.R;
 import com.developers.quickjob.quick_job.adapter_emp.AdapterPublicaciones;
+import com.developers.quickjob.quick_job.fragment_emp.fragmetn_ofertas_publicadas;
+import com.developers.quickjob.quick_job.modelo.Empresa;
 import com.developers.quickjob.quick_job.modelo.Oferta;
+import com.developers.quickjob.quick_job.restapi.VolleySingleton;
 import com.developers.quickjob.quick_job.sqlite.Operacionesbd;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,31 +37,99 @@ import java.util.List;
 public class fragment_postulaciones extends Fragment implements AdapterPublicaciones.OnItemClickOferta{
 
     LinearLayoutManager layoutManager;
-    RecyclerView publicaciones;
+    RecyclerView postulaciones;
     List<Oferta> ofertas;
     AdapterPublicaciones adaptador;
-    Operacionesbd db;
+    public static final String ID="id";
+    int idusers;
+    //Operacionesbd db;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_postulacion,container,false);
 
-        publicaciones=(RecyclerView)view.findViewById(R.id.recycler_postulaciones);
+        postulaciones=(RecyclerView)view.findViewById(R.id.recycler_postulaciones);
 
         layoutManager= new LinearLayoutManager(getActivity());
 
-        db=Operacionesbd.getInstancia(getActivity());
+        //db=Operacionesbd.getInstancia(getActivity());
 
         //ofertas=db.getOfertasEmpleo();
+        idusers=getArguments().getInt(ID);
 
-        //adaptador= new AdapterPublicaciones(getActivity(),ofertas,this);
-        publicaciones.setLayoutManager(layoutManager);
+        ofertas= new ArrayList<>();
+        adaptador= new AdapterPublicaciones(getActivity(),ofertas,this);
+        postulaciones.setLayoutManager(layoutManager);
+        postulaciones.setAdapter(adaptador);
 
-        //publicaciones.setAdapter(adaptador);
+        String url="http://unmsmquickjob.pe.hu/quickjob/oferta_postuladas.php?idpostulante="+idusers;
+        JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                procesarRespuesta(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        VolleySingleton.getInstance(getActivity()).addRequestQueue(jsonObjectRequest);
 
         return view;
     }
+
+    private void procesarRespuesta(JSONObject response) {
+        try {
+            String mensaje = response.getString("estado");
+            switch (mensaje) {
+                case "1":
+                    // Obtener objeto "meta"
+                    JSONArray jsonArray=response.getJSONArray("oferta");
+                    for (int i=0; i<jsonArray.length();i++){
+                        Oferta oferta= new Oferta();
+                        JSONObject jsonObject= jsonArray.getJSONObject(i);
+                        oferta.setId(jsonObject.getString("oferta_id"));
+                        oferta.setPuesto(jsonObject.getString("oferta_puesto"));
+                        oferta.setUbicacion_job(jsonObject.getString("oferta_ubicacion"));
+                        oferta.setFecha_publicacion(jsonObject.getString("oferta_fecha_public"));
+                        Empresa empresa= new Empresa();
+                        empresa.setNombre_comercial(jsonObject.getString("empresa_nom_comercial"));
+                        oferta.setEmpresa(empresa);
+                        Log.d(fragmetn_ofertas_publicadas.class.getName(),oferta.getPuesto());
+                        ofertas.add(oferta);
+                    }
+
+                    adaptador.notifyDataSetChanged();
+
+                    break;
+
+                case "2":
+                    String mensaje2 = response.getString("mensaje");
+                    Toast.makeText(
+                            getActivity(),
+                            mensaje2,
+                            Toast.LENGTH_LONG).show();
+                    break;
+
+                case "3":
+                    String mensaje3 = response.getString("mensaje");
+                    Toast.makeText(
+                            getActivity(),
+                            mensaje3,
+                            Toast.LENGTH_LONG).show();
+                    break;
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     @Override
     public void itemClick(Oferta app) {
