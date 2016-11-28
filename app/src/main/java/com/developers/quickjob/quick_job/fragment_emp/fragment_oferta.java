1bc8c8ff8e640e1feb73271d36c582f10d74e93f
Jhonn Ayala Chero <y2jz28@gmail.com>
2016-11-28 01:44:29 -0500

    Publicar Oferta hecha

--- app/src/main/java/com/developers/quickjob/quick_job/fragment_emp/fragment_oferta.java
+++ app/src/main/java/com/developers/quickjob/quick_job/fragment_emp/fragment_oferta.java
@@ -1,6 +1,7 @@
 package com.developers.quickjob.quick_job.fragment_emp;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,12 +17,26 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.developers.quickjob.quick_job.LoginActivity;
 import com.developers.quickjob.quick_job.R;
+import com.developers.quickjob.quick_job.RegistersEmpActivity;
+import com.developers.quickjob.quick_job.modelo.Empresa;
 import com.developers.quickjob.quick_job.modelo.Oferta;
+import com.developers.quickjob.quick_job.restapi.VolleySingleton;
 import com.developers.quickjob.quick_job.sqlite.Operacionesbd;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -62,7 +77,7 @@ public class fragment_oferta extends Fragment implements AdapterView.OnItemSelec
 
     public static final String ID = "id";
     int idemps;
-    Operacionesbd db;
+    //Operacionesbd db;
 
     String sdiponb;
     String sperfil;
@@ -75,7 +90,7 @@ public class fragment_oferta extends Fragment implements AdapterView.OnItemSelec
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_publ_oferta, container, false);
         ButterKnife.bind(this, view);
-        db = Operacionesbd.getInstancia(getActivity());
+        //db = Operacionesbd.getInstancia(getActivity());
 
         idemps = getArguments().getInt(ID);
 
@@ -109,14 +124,101 @@ public class fragment_oferta extends Fragment implements AdapterView.OnItemSelec
         Log.d(fragment_oferta.class.getName(), publicacion);
         oferta.setId_empresa(idemps);
 
-        if (db.publicarOferta(oferta)) {
+        registrarOferta(oferta);
+
+        /*if (db.publicarOferta(oferta)) {
             //databaseReference.child("ofertas").child(String.valueOf(idemps)).setValue(oferta);
             Toast.makeText(getActivity(), " Registrado", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getActivity(), "Completar datos", Toast.LENGTH_SHORT).show();
+        }*/
+
+
+    }
+
+    public void registrarOferta(Oferta oferta){
+
+        String url="http://unmsmquickjob.pe.hu/quickjob/registrar_oferta.php";
+
+        final String pst= oferta.getPuesto();
+        final String a= oferta.getArea();
+        final String t=oferta.getTipo_trabajo();
+        final String d=oferta.getDisponibilidad();
+        final String s=oferta.getSueldo();
+        final String fi=oferta.getFecha_inicio();
+        final String u=oferta.getUbicacion_job();
+        final String rs=oferta.getRequisitos();
+        final String fs=oferta.getFunciones();
+        final String p=oferta.getPerfil();
+        final String g=oferta.getGenero();
+        final String ras=oferta.getRamas_carrera();
+        final String fp=oferta.getFecha_publicacion();
+        final String ide=String.valueOf(oferta.getId_empresa());
+
+        HashMap<String,String> map = new HashMap<>();
+        map.put("pst",pst);
+        map.put("a",a);
+        map.put("t",t);
+        map.put("d",d);
+        map.put("s",t);
+        map.put("fi",fi);
+        map.put("u",u);
+        map.put("rs",rs);
+        map.put("fs",fs);
+        map.put("p",p);
+        map.put("g",g);
+        map.put("ras",ras);
+        map.put("fp",fp);
+        map.put("ide",ide);
+
+        JSONObject jsonObject= new JSONObject(map);
+
+        Log.d(RegistersEmpActivity.class.getName(),jsonObject.toString());
+
+        VolleySingleton.getInstance(getActivity()).addRequestQueue(
+                new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                procesarRespuesta(response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(RegistersEmpActivity.class.getName(), "Error Volley: " + error.getMessage());
+                    }
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String ,String> headers= new HashMap<String, String>();
+                        headers.put("Content-Type","application/json; charset=utf-8");
+                        headers.put("Accept","application/json");
+                        return headers;
+                    }
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8" + getParamsEncoding();
+                    }
+                });
+
+    }
+
+    private void procesarRespuesta(JSONObject response){
+        try {
+            String estado=response.getString("estado");
+            String mensaje= response.getString("mensaje");
+            switch (estado){
+                case "1":
+                    Toast.makeText(getActivity(),mensaje,Toast.LENGTH_LONG).show();
+                    break;
+                case "2":
+                    Toast.makeText(getActivity(),mensaje,Toast.LENGTH_LONG).show();
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-
     }
 
     @Override
@@ -152,8 +254,7 @@ public class fragment_oferta extends Fragment implements AdapterView.OnItemSelec
         DatePickerDialog datePicker = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                String date =String.valueOf(dayOfMonth)+"/"+String.valueOf(monthOfYear)
-                        +"/"+String.valueOf(year);
+                String date =String.valueOf(year)+"-"+String.valueOf(monthOfYear)+"-"+String.valueOf(dayOfMonth);
                 fecha.setText(date);
             }
         }, yy, mm, dd);
